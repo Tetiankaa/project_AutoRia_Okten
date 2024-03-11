95fd363ba671c49f23ac5e198e1dbfe0d8b950f4
Tetiankaa <tanyakorsun0208@gmail.com>
2024-03-11 16:42:10 +0100

    Initial commit

---
+++ src/main/java/autoria/ProjectAutoRiaOktenApplication.java
@@ -0,0 +1,13 @@
+package autoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAutoRiaOktenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAutoRiaOktenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/autoria/ProjectAutoRiaOktenApplicationTests.java
@@ -0,0 +1,13 @@
+package autoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAutoRiaOktenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
